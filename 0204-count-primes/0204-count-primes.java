96a0c7820ce3ab5ba3293f4ca859e714873230d1
yashtyagee <yashashvityagi182@gmail.com>
2026-07-05 15:35:32 +0530

    Time: 102 ms (60.26%), Space: 51.2 MB (37.07%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -7,9 +7,9 @@ class Solution {
         for(int i=2;i<n;i++){
             isPrime[i]=true;
         }
-        for(int i=2;i<n;i++){
+        for(int i=2;i*i<n;i++){
             if(isPrime[i]){
-                for(int j=2*i;j<n;j+=i){
+                for(int j=i*i;j<n;j+=i){
                     isPrime[j]=false;
                 }
             }
