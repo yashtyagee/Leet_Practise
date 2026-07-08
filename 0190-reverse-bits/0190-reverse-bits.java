fc51862ae0af63b39fef7b92f4cd0327f80ef8a9
yashtyagee <yashashvityagi182@gmail.com>
2026-07-08 11:45:24 +0530

    Time: 0 ms (100%), Space: 42.5 MB (52.5%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseBits(int n) {
+        int r=0;
+        for(int i=0;i<32;i++){
+            r<<=1;
+            r|=(n&1);
+            n>>>=1;
+        }
+        return r;
+
+        
+    }
+}
