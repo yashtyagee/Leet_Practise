8f4922e4931ed6d089f9ab5de82294b4ed6d3854
yashtyagee <yashashvityagi182@gmail.com>
2026-08-05 13:49:35 +0530

    Time: 3 ms (86.1%), Space: 43.3 MB (41.57%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() % 2 != 0) return false;
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else {
+                if (stack.isEmpty() || stack.pop() != c) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
