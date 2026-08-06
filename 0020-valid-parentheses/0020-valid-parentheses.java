be04b25b0292f70e713723d9fcf48c610b582ca7
yashtyagee <yashashvityagi182@gmail.com>
2026-08-06 10:18:27 +0530

    Time: 2 ms (98.29%), Space: 43.3 MB (58.34%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,24 +2,23 @@ import java.util.Stack;
 
 class Solution {
     public boolean isValid(String s) {
-        if (s.length() % 2 != 0) return false;
-
-        Stack<Character> stack = new Stack<>();
-
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
+        Stack<Character> stack=new Stack<>();
+        for(char c:s.toCharArray()){
+            if(c=='('){
                 stack.push(')');
-            } else if (c == '{') {
+            }
+            else if(c=='{'){
                 stack.push('}');
-            } else if (c == '[') {
+            }
+            else if(c=='['){
                 stack.push(']');
-            } else {
-                if (stack.isEmpty() || stack.pop() != c) {
+            }
+            else{
+                if(stack.isEmpty() || stack.pop()!=c){
                     return false;
                 }
             }
         }
-
         return stack.isEmpty();
     }
 }
