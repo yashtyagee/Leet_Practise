84d91431496d154724cabfe3dca69a1863f5d503
yashtyagee <yashashvityagi182@gmail.com>
2026-08-20 11:07:32 +0530

    Time: 7 ms (72.46%), Space: 49.8 MB (44.75%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String sortedKey = new String(chars);
+
+            map.putIfAbsent(sortedKey, new ArrayList<>());
+            map.get(sortedKey).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
