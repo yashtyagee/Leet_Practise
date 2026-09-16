aa9403016b9779b4add956e502e093fda91ae925
yashtyagee <yashashvityagi182@gmail.com>
2026-09-16 11:31:55 +0530

    Time: 60 ms (10.04%), Space: 68.6 MB (84.69%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -6,7 +6,7 @@ class Solution {
         int l=0;
         while(r<fruits.length){
             map.put(fruits[r],map.getOrDefault(fruits[r],0)+1);
-            while(map.size()>2){
+            if(map.size()>2){
                 map.put(fruits[l],map.get(fruits[l])-1);
                 if(map.get(fruits[l])==0){
                     map.remove(fruits[l]);
